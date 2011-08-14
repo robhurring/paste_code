1859f54876c2a860a2f19c6fb58d9ae0f8e07d91
Rob <robhurring@gmail.com>
2011-08-14 16:43:17 -0400

    add validations

--- app/models/paste.rb
+++ app/models/paste.rb
@@ -1,3 +1,6 @@
 class Paste < ActiveRecord::Base
-  validates :title, :presence => true
+  validates :title, :code, :public, :presence => true
+  validates :language, :presence => true, :inclusion => Languages.map(&:second)
+  
+  attr_accessible :title, :language, :code
 end
